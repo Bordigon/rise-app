c07fb611d154ec1f829171a65e0ee6930e34e104
Rafael Cardona <rcardonasanta@gmail.com>
2025-10-13 17:42:13 +0000

    login-register-profil

--- src/api/routes.py
+++ src/api/routes.py
@@ -1,22 +1,54 @@
-"""
-This module takes care of starting the API Server, Loading the DB and Adding the endpoints
-"""
-from flask import Flask, request, jsonify, url_for, Blueprint
+import bcrypt
+from flask import Blueprint, request, jsonify
+from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
 from api.models import db, User
-from api.utils import generate_sitemap, APIException
-from flask_cors import CORS
 
 api = Blueprint('api', __name__)
 
-# Allow CORS requests to this API
-CORS(api)
+@api.route('/test', methods=["GET"])
+def test():
+    return "Hola", 200
+
+@api.route('/register', methods=['POST'])
+def register():
+    data = request.get_json()
+    email = data.get("email")
+    password = str(data.get("password"))
+
+    if User.query.filter_by(email=email).first():
+        return jsonify({"msg": "User already exists"}), 400
+
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+
+    user = User(email=email, password=hashed_password.decode(
+        'utf-8'), is_active=True)
+    db.session.add(user)
+    db.session.commit()
+
+    return jsonify(user.serialize()), 201
+
+@api.route('/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    email = data.get('email')
+    password = data.get('password')
+
+    user = User.query.filter_by(email=email).first()
+    if not user or not bcrypt.checkpw(password.encode('utf-8'), user.password.encode('utf-8')):
+        return jsonify({"msg": "Bad username or password"}), 401
+
+    access_token = create_access_token(identity=str(user.id))
+    return jsonify(token=access_token, user=user.serialize()), 200
 
 
-@api.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
+@api.route('/profile', methods=['GET'])
+@jwt_required()
+def profile():
+    current_user_id = get_jwt_identity()
+    user = User.query.get(current_user_id)
 
-    response_body = {
-        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
-    }
+    if user is None:
+        return jsonify({"msg": "User not found"}), 404
 
-    return jsonify(response_body), 200
+    return jsonify(user.serialize()), 200
