24e10f92ea510027c17315128ce800ee815ec711
SantiNones <santiagonones@gmail.com>
2025-11-03 19:32:00 +0000

    feat/levelUtils.js Created new file with function to manage exp and lvl

--- src/front/layout/Layout.jsx
+++ src/front/layout/Layout.jsx
@@ -3,8 +3,9 @@ import { Outlet, useLocation } from "react-router-dom";
 import useGlobalReducer from "../hooks/useGlobalReducer.jsx";
 import AppSidebar from "../components/AppSidebar.jsx";
 import ExpBar from "../components/ExpBar.jsx";
-import PublicNavbar from "../components/PublicNavbar.jsx"; // Asegúrate de importar PublicNavbar
+import PublicNavbar from "../components/PublicNavbar.jsx"; 
 import "../styles/Layout.css"; 
+import { calculateLevelData } from "../utils/levelUtils.js"; 
 
 export default function Layout() {
     const { store } = useGlobalReducer();
@@ -13,9 +14,8 @@ export default function Layout() {
     const isProfilePage = location.pathname === '/profile';
     const isAuthPage = location.pathname === '/login' || location.pathname === '/register';
 
-    const level = store.user?.level || 1;
-    const currentExp = store.user?.xp || 0;
-    const nextLevelExp = 1000;
+    const totalExp = store.user.level? store.user.level: 0;
+    const { level, currentLevelExp, expToNextLevel } = calculateLevelData(totalExp);
 
     const mainClasses = isAuthenticated
         ? `main-with-sidebar ${isProfilePage ? 'no-padding-profile' : ''}` 
@@ -29,13 +29,12 @@ export default function Layout() {
     return (
         <div className={layoutClass}> {/* <-- USA LA CLASE DINÁMICA */}
             {isAuthenticated ? (
-                // --- VISTA PRIVADA (LOGUEADO) ---
                 <>
                     <AppSidebar user={store.user} />
                     <ExpBar
                         level={level}
-                        currentExp={currentExp}
-                        nextLevelExp={nextLevelExp}
+                        currentExp={currentLevelExp}
+                        nextLevelExp={expToNextLevel}
                     />
                 </>
             ) : (
